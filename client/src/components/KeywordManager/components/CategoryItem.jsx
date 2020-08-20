a54a0af79ffcb7fe9e11fc6234f9045d473785f4
Diogo F. Marques <diogo@fontainhas.com>
2020-08-20 17:52:33 +0100

    added new tag functionslity

--- client/src/components/KeywordManager/components/CategoryItem.jsx
+++ client/src/components/KeywordManager/components/CategoryItem.jsx
@@ -1,19 +1,52 @@
 import classNames from 'classnames';
+import { isEmpty } from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
 import Tag from '../../shared/Tag';
 import TextInput from '../../shared/TextInput';
 
 const CategoryItem = ({ categoryName, keywords }) => {
+  const containerRef = useRef();
+  const [newTag, setNewTag] = useState(false);
+  const [newTagText, setNewTagText] = useState(null);
+
+  const generateNewTag = e => {
+    if (e.target === containerRef.current && !newTag && newTagText !== "") {
+      setNewTag(true);
+      setNewTagText("")
+    } else {
+       setNewTagText(null)
+    }
+  };
+
+  const handleNewTagComplete = value => {
+    if (!value?.length) {
+       setNewTag(false);
+    }
+  };
+
   return (
     <div
+      ref={containerRef}
       className={classNames(
-        'w-full rounded-md flex flex-wrap items-center justify-start overflow-hidden p-1 border-2 border-red'
-      )}>
+        'w-full rounded-md flex flex-wrap items-center justify-start overflow-hidden p-1 border-2 border-red cursor-pointer'
+      )}
+      onClick={generateNewTag}>
       <div className='px-3 bg-smetrics rounded mr-2 mb-1'>
         <TextInput value={categoryName} className='bg-transparent text-white h-full' />
       </div>
       {keywords?.length && keywords.map((tag, i) => <Tag key={i} value={tag} className='bg-black h-7 mr-2 mb-1' />)}
+      {newTag && (
+        <Tag
+          value={newTagText}
+          onChange={setNewTagText}
+          className='bg-black h-7 mr-2 mb-1'
+          autoFocus
+          onComplete={handleNewTagComplete}
+        />
+      )}
     </div>
   );
 };
--- client/src/components/shared/Tag.jsx
+++ client/src/components/shared/Tag.jsx
@@ -1,15 +1,26 @@
 import classNames from 'classnames';
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useEffect, useRef } from 'react';
+import isFunction from 'lodash/isFunction';
 
-const Tag = ({ className, value }) => {
+const Tag = ({ className, value, autoFocus, onComplete }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (autoFocus) inputRef.current.focus();
+  }, []);
+
   return (
     <div className={classNames('rounded-lg', className)}>
       <span
+        ref={inputRef}
         className='text-white outline-none px-3 pt-1 pb-2 leading-none flex items-center justify-between'
         role='textbox'
-        contenteditable='true'>
-        <p>{value}</p>
+        suppressContentEditableWarning
+        contentEditable='true'
+        spellCheck={false}
+        onBlur={e => isFunction(onComplete) && onComplete(value, e)}>
+        {value}
       </span>
     </div>
   );
